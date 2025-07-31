088564ea497afb40bf9e80afbbf6b02159f17542
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-07-31 17:52:56 +0200

    build: :tada: Init Project spring.io

---
+++ src/main/java/com/teo/client_dashboard/ClientDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.teo.client_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientDashboardApplication.class, args);
+	}
+
+}
